70b8b7e2913ca3f7b869d5fdbd1a7ec36427c7bb
song.liu <song.liu@ele.me>
2016-10-18 14:44:41 +0800

    practice

--- practice/src/main/java/com/song/bean/BeanA.java
+++ practice/src/main/java/com/song/bean/BeanA.java
@@ -1,11 +1,25 @@
 package com.song.bean;
 
 /**
- * TODO
+ * switch中String的实现原理
  *
  * @author song@liu@ele.me
  * @date 2016-10-14 15:26
  */
 public class BeanA {
-
+    public int strSwitchTest(String key) {
+        int flag;
+        switch (key){
+            case "你好啊，今天天气不错呢":
+                flag = 0;
+                break;
+            case "b":
+                flag = 1;
+                break;
+            default:
+                flag = 2;
+                break;
+        }
+        return flag;
+    }
 }
---
+++ practice/src/main/java/java/lang/String.java
@@ -0,0 +1,13 @@
+package java.lang;
+
+/**
+ * jvm优先加载jdk中java.lang.String会覆盖当前类
+ *
+ * @author song@liu@ele.me
+ * @date 2016-10-18 14:40
+ */
+public class String {
+    public static void main(String[] args){
+        System.out.println("sfvsvs");
+    }
+}
